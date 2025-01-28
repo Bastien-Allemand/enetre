de7c6665f2de8a0112e7e11f27889ce57d9582cf
Bastien Allemand--Leung Pin <ballemandleungpin@gaming.tech>
2025-01-28 12:18:43 +0100

    z

--- src/enetre/Scenes/Scene1.cpp
+++ src/enetre/Scenes/Scene1.cpp
@@ -13,17 +13,46 @@ void Scene1::addEntity(Entity* _entity)
 
 void Scene1::Init()
 {
+	Player* player = new Player();
+	player->Init();
+	addEntity(player);
 
 }
 
 void Scene1::Update()
 {
+	for (Entity* entity : m_entities)
+	{
+		entity->Update();
+	}
+
 }
 
 void Scene1::Render()
 {
+	for (Entity* entity : m_entities)
+	{
+		entity->Render();
+	}
 }
 
 void Scene1::Destroy()
 {
+	for (Entity* entity : m_entities)
+	{
+		entity->Destroy();
+	}
+}
+
+template<class T>
+inline void Scene1::removeEntitybytype()
+{
+	for (int i = 0; i < m_entities.size(); i++)
+	{
+		if (T* s = dynamic_cast<T*>(m_entities[i]))
+		{
+			delete s;
+			m_entities.erase(m_entities.begin() + i);
+		}
+	}
 }
--- src/enetre/Scenes/Scene1.h
+++ src/enetre/Scenes/Scene1.h
@@ -9,9 +9,16 @@ public:
 	Scene1();
 	~Scene1();
 	void addEntity(Entity* _entity);
+	template<class T>
+	T* GetEntity();
+	template<class T>
+	std::vector<T*> GetAllEntity();
+	template<class T>
+	void removeEntitybytype();
 	void Init() override;
 	void Update() override;
 	void Render() override;
 	void Destroy() override;
 };
+
 
